07ac5f4fe7b381ce829b736588e6984045cba682
“Marc <marc_piantone@live.fr>
2020-06-16 15:38:45 -0400

    Answer exercise-2

--- workshop/exercise-2.js
+++ workshop/exercise-2.js
@@ -3,7 +3,12 @@
 // the greater one.
 
 // Define the function
-function max(num1, num2) {}
+function max(num1, num2) {
+    if (num1 <num2 == true) {
+        return num2
+    }
+    else return num1
+}
 
 // Call the function
-max(12, 43);
+console.log(max(12, 43))
